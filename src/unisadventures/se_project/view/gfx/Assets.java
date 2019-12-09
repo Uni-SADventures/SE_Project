2e0bfb91546e3fdceccf1c73d8be957ed04a3d0c
Emanuela <Emanuela@172.19.214.119>
2019-12-09 15:10:06 +0100

    Added images and updated Assets class

--- src/unisadventures/se_project/view/gfx/Assets.java
+++ src/unisadventures/se_project/view/gfx/Assets.java
@@ -9,13 +9,34 @@ public class Assets {
 	
 	private static final int width = 32, height = 32;
 	
-	public static BufferedImage player, scenario;
+	public static BufferedImage objects, tilesheet, player, scenario, ground, ground_1, grass, tree, enemy, CFU, heart, heart_1, heart_2;
 
 	public static void init(){
-                player = ImageLoader.loadImage("resources/images/char.png");
-       
+                player = ImageLoader.loadImage("resources/images/character_sprite.png");
+                player = player.getSubimage(48,0,16,height);
+                
                 
                 scenario = ImageLoader.loadImage("resources/images/scenario.jpg") ; 
+               
+                
+                tilesheet = ImageLoader.loadImage("resources/images/tilesheet1.png");
+                ground = tilesheet.getSubimage(0, 0, width, height);
+                ground_1 = tilesheet.getSubimage(64, 0, width, height);
+                grass = tilesheet.getSubimage(0, 176, width, 16);
+                tree = tilesheet.getSubimage(64, 128, width*2, height*2);
+                
+                
+                enemy = ImageLoader.loadImage("resources/images/enemy_sprite.png");
+                enemy= enemy.getSubimage(16, 96, 16, height);
+                
+                
+                objects=ImageLoader.loadImage("resources/images/objectstilesheet.png");
+                CFU = objects.getSubimage(160, 0, width, height);
+                heart = objects.getSubimage(0, 0, width, height);
+                heart_1 = objects.getSubimage(32, 0, width, height);
+                heart_2 = objects.getSubimage(64, 0, width, height);
+                
+                
 		//SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("res/textures/char.png"));
             
 		//SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("res/textures/sheet.png"));
