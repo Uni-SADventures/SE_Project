80596bdd1c51b3d5ef8e08998243da77c4a84d70
Emilio Picariello <emiliopicariello97@gmail.com>
2019-12-11 16:19:05 +0100

    Added UI fixed java sdk 13

--- src/unisadventures/se_project/view/gfx/Assets.java
+++ src/unisadventures/se_project/view/gfx/Assets.java
@@ -12,15 +12,16 @@ public class Assets {
 
     private static final int width = 32, height = 32;
 
-    public static BufferedImage objects, tilesheet, player, scenario, ground, ground_1, grass, tree, enemy, CFU, heart, heart_1, heart_2;
-    public static BufferedImage rock,sky,limit;
+    public static BufferedImage objects, tilesheet, player, scenario, ground, ground_1, grass, tree, enemy, CFU, full_heart, half_heart, empty_heart;
+    public static BufferedImage rock, sky, limit;
+    public static BufferedImage zero, one,two,three,four,five,six,seven,eight,nine;
     /*@author Emanuela Paolo 
     this method loads the sprites of the player, enemy and the main objects and tiles.
      */
     public static void init() {
         //load player,scenario and enemy
         player = ImageLoader.loadImage("resources/images/char.png");
-   //     player = ImageLoader.loadImage("resources/images/character_sprite.png").getSubimage(48, 0, 16, height);
+        //     player = ImageLoader.loadImage("resources/images/character_sprite.png").getSubimage(48, 0, 16, height);
         scenario = ImageLoader.loadImage("resources/images/scenario.jpg");
         enemy = ImageLoader.loadImage("resources/images/enemy_sprite.png").getSubimage(16, 96, 16, height);
 
@@ -28,19 +29,26 @@ public class Assets {
         tilesheet = ImageLoader.loadImage("resources/images/tilesheet1.png");
         ground = tilesheet.getSubimage(0, 0, width, height);
         ground_1 = tilesheet.getSubimage(64, 0, width, height);
-        grass = tilesheet.getSubimage(0, 176, width, width/2);
+        grass = tilesheet.getSubimage(0, 176, width, width / 2);
         tree = tilesheet.getSubimage(64, 128, width * 2, height * 2);
 
         //load objects
         objects = ImageLoader.loadImage("resources/images/objectstilesheet.png");
         CFU = objects.getSubimage(160, 0, width, height);
-        heart = objects.getSubimage(0, 0, width, height);
-        heart_1 = objects.getSubimage(32, 0, width, height);
-        heart_2 = objects.getSubimage(64, 0, width, height);
-        
-        
-        
-        
+        full_heart = objects.getSubimage(0, 0, width, height);
+        half_heart = objects.getSubimage(32, 0, width, height);
+        empty_heart = objects.getSubimage(64, 0, width, height);
+        zero = ImageLoader.loadImage("resources/images/numbers/Number0.png");
+        one = ImageLoader.loadImage("resources/images/numbers/Number1.png");
+        two = ImageLoader.loadImage("resources/images/numbers/Number2.png");
+        three = ImageLoader.loadImage("resources/images/numbers/Number3.png");
+        four = ImageLoader.loadImage("resources/images/numbers/Number4.png");
+        five = ImageLoader.loadImage("resources/images/numbers/Number5.png");
+        six = ImageLoader.loadImage("resources/images/numbers/Number6.png");
+        seven = ImageLoader.loadImage("resources/images/numbers/Number7.png");
+        eight = ImageLoader.loadImage("resources/images/numbers/Number8.png");
+        nine = ImageLoader.loadImage("resources/images/numbers/Number9.png");
+
         //temp
         //to be correctly ordered
         SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("resources/images/grass.png"));
@@ -49,9 +57,6 @@ public class Assets {
         rock = sheet.crop(0, 0, 60, 60);
         sky = sheet1.crop(0, 0, 1000, 375);
         limit = sheet1.crop(0, 375, 1000, 375);
-
-        
-        
 
     }
 }
--- src/unisadventures/se_project/view/gfx/StateGraphics.java
+++ src/unisadventures/se_project/view/gfx/StateGraphics.java
@@ -59,8 +59,84 @@ public class StateGraphics {
     
     
     public void renderUi(Graphics g, int health, int maxHealth, int cfu, int lives){
-       // for(){
-       // }
-       // g.drawImage(Assets.scenario, (int) -_handler.getCam().getxOffset(), (int) -_handler.getCam().getyOffset(), null);
-    }
+       g.drawImage(Assets.CFU,50,10,null);
+       int j=35;
+
+        boolean odd = (health %2 == 0) ;
+        
+        int pixelIncrementer = 1 ;
+        for(int i = 2 ; i <= maxHealth; i += 2){
+            if(health > 1)
+                 g.drawImage(Assets.full_heart, 55 + (j * pixelIncrementer), 10, null);
+            else if(health == 1){
+                g.drawImage(Assets.half_heart, 55 + (j * pixelIncrementer), 10, null);
+            } else
+                g.drawImage(Assets.empty_heart, 55 + (j * pixelIncrementer), 10, null);
+            health -= 2 ;
+            pixelIncrementer++ ;    
+        }
+        cfu=20;
+        int[] cfuDigits = String.valueOf(cfu).chars().map(Character::getNumericValue).toArray();
+        
+        pixelIncrementer = 0;
+        for (int i = 0; i < cfuDigits.length; i++) {
+           
+            if (cfuDigits[i] == 0) {
+                g.drawImage(Assets.zero, 10+pixelIncrementer, 10, null);
+            } else if (cfuDigits[i] == 1) {
+                g.drawImage(Assets.one, 10+pixelIncrementer, 10, null);
+            } else if (cfuDigits[i] == 2) {
+                g.drawImage(Assets.two, 10+pixelIncrementer, 10, null);
+            } else if (cfuDigits[i] == 3) {
+                g.drawImage(Assets.three, 10+pixelIncrementer, 10, null);
+            } else if (cfuDigits[i] == 4) {
+                g.drawImage(Assets.four, 10+pixelIncrementer, 10, null);
+            } else if (cfuDigits[i] == 5) {
+                g.drawImage(Assets.five, 10+pixelIncrementer, 10, null);
+            } else if (cfuDigits[i] == 6) {
+                g.drawImage(Assets.six, 10+pixelIncrementer, 10, null);
+            } else if (cfuDigits[i] == 7) {
+                g.drawImage(Assets.seven, 10+pixelIncrementer, 10, null);
+            } else if (cfuDigits[i] == 8) {
+                g.drawImage(Assets.eight, 10+pixelIncrementer, 10, null);
+            } else if (cfuDigits[i] == 9) {
+                g.drawImage(Assets.nine, 10+pixelIncrementer, 10, null);
+            }
+               pixelIncrementer +=7;
+        }
+        lives=20;
+        pixelIncrementer +=0;
+        int[] livesDigits = String.valueOf(lives).chars().map(Character::getNumericValue).toArray();
+        for (int i = 0; i < livesDigits.length; i++) {
+           
+            if (livesDigits[i] == 0) {
+                g.drawImage(Assets.zero, 200+pixelIncrementer, 10, null);
+            } else if (livesDigits[i] == 1) {
+                g.drawImage(Assets.one, 200+pixelIncrementer, 10, null);
+            } else if (livesDigits[i] == 2) {
+                g.drawImage(Assets.two, 200+pixelIncrementer, 10, null);
+            } else if (livesDigits[i] == 3) {
+                g.drawImage(Assets.four, 200+pixelIncrementer, 10, null);
+            } else if (livesDigits[i] == 5) {
+                g.drawImage(Assets.five, 200+pixelIncrementer, 10, null);
+            } else if (livesDigits[i] == 6) {
+                g.drawImage(Assets.six, 200+pixelIncrementer, 10, null);
+            } else if (livesDigits[i] == 7) {
+                g.drawImage(Assets.seven, 200+pixelIncrementer, 10, null);
+            } else if (livesDigits[i] == 8) {
+                g.drawImage(Assets.eight, 200+pixelIncrementer, 10, null);
+            } else if (livesDigits[i] == 9) {
+                g.drawImage(Assets.nine, 200+pixelIncrementer, 10, null);
+            }
+               pixelIncrementer +=7;
+        }
+        
+        g.drawImage(Assets.full_heart, 210+pixelIncrementer, 10, null);
+            //g.setColor(Color.red);
+            //g.drawString("10", 15 + (j * 2), 50);
+        
+        
+        
+        }
+    
 }
